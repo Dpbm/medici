4179576ce6f2b21816a20c2a05940d2d6c8a7745
Gabriela Stela <gabrielastela13@outlook.com>
2024-09-27 22:18:57 -0300

    adding database

---
+++ medici/lib/utils/db.dart
@@ -0,0 +1,44 @@
+import 'package:path/path.dart';
+//import 'package:seshat/models/note.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<Database> getDB() async {
+  final database = openDatabase(join(await getDatabasesPath(), 'data.db'),
+      onCreate: (db, version) {
+    db.execute(
+      '''
+        CREATE TABLE drug (
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          name TEXT NOT NULL, 
+          image TEXT, 
+          expiration_date INTEGER, 
+          quantity NUMERIC, 
+          dose_type TEXT, 
+          dose NUMERIC, 
+          leaflet TEXT)
+      ''');
+
+    db.execute(
+      '''
+        CREATE TABLE alert(
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          time INTEGER,
+          drug_id INTEGER,
+          FOREIGN KEY(drug_id) REFERENCES drug(id)
+        )
+      ''');
+
+    db.execute(
+      '''
+        CREATE TABLE notification(
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          expiration_offset INTEGER,
+          quantity_offset NUMERIC,
+          drug_id INTEGER,
+          FOREIGN KEY(drug_id) REFERENCES drug(id)
+        )
+      ''');
+  }, version: 1);
+
+  return database;
+}
