c5d52a5b3c566e596537c49c43c63e4e1f098483
Dpbm <dpbm136@gmail.com>
2024-12-09 13:28:15 -0300

    updated leaflet code

--- medici/lib/utils/leaflet.dart
+++ medici/lib/utils/leaflet.dart
@@ -1,11 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+String getApiURL(String drugName) {
+  return "https://leaflet-alpha-six.vercel.app/api/medicine/$drugName";
+}
+
 Future<String?> getLeaflet(String drugName) async {
-  final http.Response data = await http.get(Uri.parse(
-      "https://leaflet-alpha-six.vercel.app/api/medicine/" + drugName));
-
+  final http.Response data = await http.get(Uri.parse(getApiURL(drugName)));
   final leafletData = await json.decode(data.body);
-
   return leafletData['pdf_url'];
 }
